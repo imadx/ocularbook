edb72043cf54e89b5730d64ed2670112bf3b1934
Ishan Madhusanka <ahtimadhusanka@gmail.com>
2022-06-11 21:49:26 +0530

    allow non-strict instead of server.fs.allow

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from "vite";
+import { defineConfig, searchForWorkspaceRoot } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  server: {
+    fs: {
+      strict: false,
+    },
+  },
 });
