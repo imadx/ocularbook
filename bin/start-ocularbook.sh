4b17909d365562d9e8ff5c339a45a8a16f92c850
Ishan Madhusanka <ahtimadhusanka@gmail.com>
2022-06-11 21:54:35 +0530

    fix bin command

--- bin/start-ocularbook.sh
+++ bin/start-ocularbook.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-VITE_STORIES_BASE_DIRECTORY=/tmp/stories
-ln -s $VITE_STORIES_BASE_DIRECTORY ./external-stories  
-
-VITE_OCULARBOOK_SOURCES=$output npm run dev
+(
+  cd node_modules/ocularbook
+  
+  VITE_STORIES_BASE_DIRECTORY=/tmp/stories
+  rm -f ./external-stories-aimevoli
+  ln -s $VITE_STORIES_BASE_DIRECTORY ./external-stories-aimevoli
+  
+  VITE_OCULARBOOK_SOURCES=$output npm run dev
+)
