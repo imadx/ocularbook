d62dc510a9139a6ed404a8cacbea48689376fc43
Ishan Madhusanka <ahtimadhusanka@gmail.com>
2022-06-12 15:29:49 +0530

    update glob pattern for stories

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,5 @@
 export const getFiles = async (): Promise<string[]> => {
-  const internalSources = await import.meta.glob("../../**/*.stories.svelte");
+  const internalSources = await import.meta.glob("/**/*.stories.svelte");
 
   return [...Object.keys(internalSources)];
 };
