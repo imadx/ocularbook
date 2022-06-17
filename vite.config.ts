acb514a7b5b682fc5065c05f16f829580b5bfee9
Ishan Madhusanka <ahtimadhusanka@gmail.com>
2022-06-17 14:18:53 +0530

    add support to watch for story updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,28 @@
-import { defineConfig, searchForWorkspaceRoot } from "vite";
+import { defineConfig, PluginOption, searchForWorkspaceRoot } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [svelte(), watchStoryFileUpdates()],
   server: {
     fs: {
       strict: false,
     },
   },
 });
+
+export function watchStoryFileUpdates(): PluginOption {
+  const pluginOption: PluginOption = {
+    name: "ocular-watch-story-file-updates",
+    configureServer: (server) => {
+      const handleFileChange = () => {
+        server.ws.send({ type: "full-reload" });
+      };
+
+      server.watcher.on("add", handleFileChange);
+      server.watcher.on("unlink", handleFileChange);
+    },
+  };
+
+  return pluginOption;
+}
